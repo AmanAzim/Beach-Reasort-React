f86b3d9fbb68f3316932f24aa20e5a323a4270d5
Aman <rumman.djuice1@gmail.com>
2019-07-08 18:15:02 +0200

    "Route and Navbar setup"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+//import './App.css';
+import {Route, Switch} from 'react-router-dom'
+
+import Navbar from './components/views/Navbar'
 import Home from './components/Home'
 import Rooms from './components/Rooms'
 import SingleRoom from './components/SingleRoom'
 import ErrorPage from './components/ErrorPage'
-//import './App.css';
+
+
 
 function App() {
   return (
     <React.Fragment>
-      <Home/>
-      <Rooms/>
-      <SingleRoom/>
-      <ErrorPage/>
+        <Navbar/>
+        <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/single-room/:slug" exact component={SingleRoom}/>
+            <Route path="/rooms" exact component={Rooms}/>
+            <Route component={ErrorPage}/>
+        </Switch>
     </React.Fragment>
   );
 }
---
+++ src/components/views/Navbar.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import { FaBars } from "react-icons/fa"//https://react-icons.netlify.com/#/
+import logo from '../../images/logo.svg'
+import {Link} from 'react-router-dom'
+import '../../App.css'
+
+class Navbar extends Component {
+    state={
+        isOpen:false
+    }
+
+    handleToggle=()=>{
+        this.setState({isOpen:!this.state.isOpen});
+    }
+    render() {
+        return (
+            <nav className="navbar">
+               <div className="nav-center">
+                   <div className="nav-header">
+                        <img src={logo} alt="Beach Resort"/>
+                        <button type="button" className="nav-btn" onClick={this.handleToggle}>
+                            <FaBars className="nav-icon"/>
+                        </button>
+                   </div>
+                   <ul className={this.state.isOpen? "nav-links show-nav":"nav-links"}>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/rooms">Rooms</Link>
+                        </li>
+                   </ul>
+               </div>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
